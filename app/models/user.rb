9a730bbecd3687ff6c8cb23d3d4f382dbb53946d
abeyuyajp <yellowmonkey0618@gmail.com>
2021-04-07 15:41:01 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname, length: { maximum: 20 }
+  end
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "半角英数字にしてください", on: :create } do
+    validates :password
+    validates :password_confirmation
+  end
 end
