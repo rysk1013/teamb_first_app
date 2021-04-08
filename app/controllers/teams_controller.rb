1c0505e9cf8ca90b2e70dfaafdc587781ab83d11
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-04-09 00:56:49 +0900

    Teamコントローラー；投稿機能の記述

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,24 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.create(team_params)
+    if @team.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team)
+    .permit(:name, :intro, :content, :team_lim, :member_lim)
+    .merge(user_id: current_user.id)
+  end
 end
