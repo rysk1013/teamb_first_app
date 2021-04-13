5fbaafe9972cd6e71151b87f2a9dade8cf907a56
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-04-14 06:21:06 +0900

    コントローラーアクションの修正（コメントアウト）

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,28 +1,46 @@
-class ProjectsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+# class ProjectsController < ApplicationController
+#   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+#   before_action :new_project, only: [:new, :create]
 
-  def index
-    @projects = Projects.includes(:user).order(created_at: :DESC)
-  end
+#   def index
+#     @projects = Projects.includes(:user).order(created_at: :DESC)
+#   end
 
-  def new
-    @project = project.new
-  end
+#   def new
+#   end
 
-  def create
-    @project = Projects.create(project_params)
-    if @project.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+#   def create
+#     if @project.save
+#       redirect_to root_path
+#     else
+#       render :new
+#     end
+#   end
 
-  private
+#   def show
+#   end
 
-  def project_params
-    params.require(:project)
-    .permit(:name, :intro, :content, :project_lim, :member_lim)
-    .merge(user_id: current_user.id)
-  end
-end
+#   def edit
+#   end
+
+#   def update
+#     @project.update(item_params)
+#     if @project.save
+#       redirect_to action: :show
+#     else
+#       render :edit
+#     end
+#   end
+
+#   private
+
+#   def project_params
+#     params.require(:project)
+#     .permit(:name, :intro, :content, :project_lim, :member_lim)
+#     .merge(user_id: current_user.id)
+#   end
+
+#   def new_project
+#     @project = project.new
+#   end
+# end
