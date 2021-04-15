c0da96047704c24815894d3d4a32e4f56a750169
Tomoaki-git <77450533+Tomoaki-git@users.noreply.github.com>
2021-04-15 19:25:14 +0900

    コントローラーアクションの修正

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,46 +1,19 @@
-# class ProjectsController < ApplicationController
-#   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-#   before_action :new_project, only: [:new, :create]
+class ProjectsController < ApplicationController
 
-#   def index
-#     @projects = Projects.includes(:user).order(created_at: :DESC)
-#   end
+  def index
+  end
 
-#   def new
-#   end
+  def new
+  end
 
-#   def create
-#     if @project.save
-#       redirect_to root_path
-#     else
-#       render :new
-#     end
-#   end
+  def create
+  end
 
-#   def show
-#   end
+  def show
+  end
 
-#   def edit
-#   end
+  def edit
+  end
 
-#   def update
-#     @project.update(item_params)
-#     if @project.save
-#       redirect_to action: :show
-#     else
-#       render :edit
-#     end
-#   end
-
-#   private
-
-#   def project_params
-#     params.require(:project)
-#     .permit(:name, :intro, :content, :project_lim, :member_lim)
-#     .merge(user_id: current_user.id)
-#   end
-
-#   def new_project
-#     @project = project.new
-#   end
-# end
+  def update
+  end
