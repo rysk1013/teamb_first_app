78c2c0cd0a2d756daeca96a45b07d03da1247c2c
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-04-21 11:21:31 +0900

    バリデーションを設定

--- app/models/join_list.rb
+++ app/models/join_list.rb
@@ -3,5 +3,10 @@ class JoinList < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
+  with_options presence: true do
+    validates :user_id, uniqueness: { scope: :project_id }
+    validates :project_id, uniqueness: { scope: :user_id }
+  end
+
   
 end
